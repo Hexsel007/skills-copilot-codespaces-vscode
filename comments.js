619b6b14b556da339d2069fefc0c6faf651775ab
Anderson Hexsel <anderson.hexsel@gmail.com>
2025-02-01 00:19:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// create new webserver
+var server = require('webserver').create();
+var service = server.listen(8080, function(request, response) {
+    response.statusCode = 200;
+    response.write('<html><body>Hello!</body></html>');
+    response.close();
+});
